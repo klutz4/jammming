4970b74c946ea00077f75d62dc743a5d0cd8b8af
Kelly Lutz <kellylutz@Kellys-MacBook-Air.local>
2024-06-24 13:21:34 -0600

    work on SearchBar and add fake data

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { get } from './mockBackend/fetch';
 
-function SearchBar() {
+function SearchBar(props) {
+    const [input, setInput] = useState('');
+
+    const handleInputChange = event => {
+        setInput(event.target.value);
+    };
+
     return (
         <div className='search-bar'>
-            <h2>Search here</h2>
-            <forms>
-                <input type='text'></input>
-                <button>Search</button>
-            </forms>
+            <h2>Search for your favorite song!</h2>
+            <input placeholder='Enter your favorite song' value={input} onChange={handleInputChange} type='text' />
+            <button className='search-button' >Search</button>
         </div>
     )
 };
---
+++ src/mockBackEnd/data.js
@@ -0,0 +1,44 @@
+// fake data
+
+const songs = [
+    {
+      title: "Title 1",
+      artist: "Artist 1",
+      album: "Album 1"
+    },
+    {
+        title: "Title 2",
+        artist: "Artist 2",
+        album: "Album 2"
+      },
+      {
+        title: "Title 3",
+        artist: "Artist 3",
+        album: "Album 3"
+      },
+      {
+        title: "Title 4",
+        artist: "Artist 4",
+        album: "Album 4"
+      },
+      {
+        title: "Title 5",
+        artist: "Artist 5",
+        album: "Album 5"
+      },
+      {
+        title: "Title 1",
+        artist: "Artist 1",
+        album: "Album 1"
+      },
+      {
+        title: "Title 6",
+        artist: "Artist 6",
+        album: "Album 6"
+      },
+      {
+        title: "Title 7",
+        artist: "Artist 7",
+        album: "Album 7"
+      },
+  ]
