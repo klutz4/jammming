b0f87f7aaf4f11b5b2982ea7e5914a78818ce694
Kelly Lutz <kellylutz@Kellys-MacBook-Air.local>
2024-06-24 15:31:37 -0600

    add id to fake data

--- src/mockBackEnd/data.js
+++ src/mockBackEnd/data.js
@@ -4,41 +4,49 @@ const songs = [
     {
       title: "Title 1",
       artist: "Artist 1",
-      album: "Album 1"
+      album: "Album 1",
+      id: '1',
     },
     {
         title: "Title 2",
         artist: "Artist 2",
-        album: "Album 2"
+        album: "Album 2",
+        id: '2',
       },
       {
         title: "Title 3",
         artist: "Artist 3",
-        album: "Album 3"
+        album: "Album 3",
+        id: '3',
       },
       {
         title: "Title 4",
         artist: "Artist 4",
-        album: "Album 4"
+        album: "Album 4",
+        id: '4',
       },
       {
         title: "Title 5",
         artist: "Artist 5",
-        album: "Album 5"
+        album: "Album 5",
+        id: '5',
       },
       {
         title: "Title 6",
         artist: "Artist 6",
-        album: "Album 6"
+        album: "Album 6",
+        id: '6',
       },
       {
         title: "Title 7",
         artist: "Artist 7",
-        album: "Album 7"
+        album: "Album 7",
+        id: '7',
       },
       {
         title: "Title 8",
         artist: "Artist 8",
-        album: "Album 8"
+        album: "Album 8",
+        id: '8',
       },
   ]
